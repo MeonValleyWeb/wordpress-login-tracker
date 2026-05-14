0fc4681c7acbf3e3b4592badbd9b79e364e9fc22
Andy Wilkinson <andrew@meonvalleyweb.com>
2026-05-14 13:20:55 +0100

    fix: resolve PHPCS errors in uninstall.php
    - Add proper spacing around ! operator and parentheses
    - Add periods to end of inline comments
    - Use tabs for indentation
    - Use spaces around array operators
    - Add proper spacing in function calls

--- uninstall.php
+++ uninstall.php
@@ -5,26 +5,26 @@
  * @package Custom_Login_Tracker
  */
 
-// If uninstall not called from WordPress, exit
-if (!defined('WP_UNINSTALL_PLUGIN')) {
-    exit;
+// If uninstall not called from WordPress, exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
 }
 
-// Get options to check if data should be removed
-$options = get_option('custom_login_tracker_options');
-$remove_data = isset($options['remove_data_on_uninstall']) ? $options['remove_data_on_uninstall'] : false;
+// Get options to check if data should be removed.
+$options     = get_option( 'custom_login_tracker_options' );
+$remove_data = isset( $options['remove_data_on_uninstall'] ) ? $options['remove_data_on_uninstall'] : false;
 
-// Only remove data if the option is enabled
-if ($remove_data) {
-    global $wpdb;
-    
-    // Delete tables
-    $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}custom_login_tracker");
-    $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}custom_login_tracker_failed");
-    
-    // Delete options
-    delete_option('custom_login_tracker_options');
-    
-    // Clear scheduled hooks
-    wp_clear_scheduled_hook('custom_login_tracker_cleanup');
+// Only remove data if the option is enabled.
+if ( $remove_data ) {
+	global $wpdb;
+
+	// Delete tables.
+	$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}custom_login_tracker" );
+	$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}custom_login_tracker_failed" );
+
+	// Delete options.
+	delete_option( 'custom_login_tracker_options' );
+
+	// Clear scheduled hooks.
+	wp_clear_scheduled_hook( 'custom_login_tracker_cleanup' );
 }
